e4032cb64ba486890b181f7a62a42ee0cbd88340
Marcin Krueger <marcinkrueger@gmail.com>
2022-11-04 23:56:22 +0100

    Add regex for tweets div

--- frontend/src/pages/content/components/Demo/app.tsx
+++ frontend/src/pages/content/components/Demo/app.tsx
@@ -2,7 +2,11 @@ import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
-    console.log(window.location.href);
+    const urlRegexp = new RegExp("^https\\:\\/\\/twitter\\.com.*");
+    if (window.location.href.match(urlRegexp)) {
+      let tweets = document.querySelectorAll("[data-testid='tweet']");
+      console.log(tweets);
+    }
     console.log("content view loaded");
   }, []);
 
