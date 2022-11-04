79ee634ec0cc8ad697958e6c6a5abdb21bd1582e
Marcin Krueger <marcinkrueger@gmail.com>
2022-11-04 22:33:32 +0100

    MI NIE DZIALA TWITTER

--- frontend/src/pages/content/components/Demo/app.tsx
+++ frontend/src/pages/content/components/Demo/app.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
+    console.log(window.location.href);
     console.log("content view loaded");
   }, []);
 
