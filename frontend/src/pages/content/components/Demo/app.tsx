1a43fa6e03218195c71c285cd097463200903f59
PawelZ4krzewski <61842320+PawelZ4krzewski@users.noreply.github.com>
2022-11-05 01:46:28 +0100

    nie dziala ale nie mam sil

--- frontend/src/pages/content/components/Demo/app.tsx
+++ frontend/src/pages/content/components/Demo/app.tsx
@@ -1,13 +1,30 @@
 import { useEffect } from "react";
 
 export default function App() {
+
+  // window.addEventListener('load', () => {
+  //   const urlRegexp = new RegExp("^https\\:\\/\\/twitter\\.com.*");
+  //   console.log("XDDDD")
+  //   if (window.location.href.match(urlRegexp)) {
+  //     let tweets = document.querySelectorAll("[data-testid='tweet']");
+  //     console.log(tweets);
+  //   }
+  // })
+  setTimeout(() => {
+   debugger;
+    
+  } ,2000);
+  
+  
+
+  document.addEventListener("resize", () => {
+    console.log("hihihaha");
+    // document.querySelector("[aria-label='Timeline: Explore'] > div");
+    // console.log(container.children) //container.children.length
+  })
+
   useEffect(() => {
-    const urlRegexp = new RegExp("^https\\:\\/\\/twitter\\.com.*");
-    if (window.location.href.match(urlRegexp)) {
-      let tweets = document.querySelectorAll("[data-testid='tweet']");
-      console.log(tweets);
-    }
-    console.log("content view loaded");
+    console.log("xdDUP");
   }, []);
 
   return <div>Hello world</div>;
